c6f66d06670e02371020352b994da7ad529f651b
Juan Patricio Marshall <juan.pmarshall@hotmail.com>
2016-04-07 16:04:33 -0300

    Saco los debugs y settings

--- Cliente/Debug/src/Cliente/MessageFactory.d
+++
@@ -1,6 +0,0 @@
-src/Cliente/MessageFactory.d: ../src/Cliente/MessageFactory.cpp \
- ../src/Cliente/MessageFactory.h ../src/Cliente/../Utils/TiposDefinidos.h
-
-../src/Cliente/MessageFactory.h:
-
-../src/Cliente/../Utils/TiposDefinidos.h:
--- Cliente/Debug/src/Utils/pugixml/pugixml.d
+++
@@ -1,6 +0,0 @@
-src/Utils/pugixml/pugixml.d: ../src/Utils/pugixml/pugixml.cpp \
- ../src/Utils/pugixml/pugixml.hpp ../src/Utils/pugixml/pugiconfig.hpp
-
-../src/Utils/pugixml/pugixml.hpp:
-
-../src/Utils/pugixml/pugiconfig.hpp:
